9f58b863d675acfd1ae8a44bfbdad33214dfc2b2
小k <87127041+gylwj@users.noreply.github.com>
2022-08-12 13:37:38 +0800

    Update 666.js

--- 666.js
+++ 666.js
@@ -1,4 +1,4 @@
 setInterval(function(){check()},1000);var check=function(){function
 doCheck(a){if((""+a/a)["length"]!==1||a%20===0){(function(){}["constructor"]("debugger")())}else{(function(){}["constructor"]("debugger")())}doCheck(++a)}try{doCheck(0)}catch(err){}};check();window.οncοntextmenu=function(){return
 false};window.onkeydown=window.onkeyup=window.onkeypress=function(){window.event.returnValue=false;return false};var
-h=window.innerHeight,w=window.innerWidth;window.onresize=function(){if(h!=window.innerHeight||w!=window.innerWidth){window.location="about:blank"}};
+h=window.innerHeight,w=window.innerWidth;//window.onresize=function(){if(h!=window.innerHeight||w!=window.innerWidth){window.location="about:blank"}};//
